33042185d60e8f211bb6742b8d857020983c081b
librariandad <kengwall@gmail.com>
2025-05-17 08:54:03 -0500

    added frame rate limiter

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +19,7 @@ def main():
         screen.fill((0, 0, 0))
         pygame.Surface.fill(screen,(0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
     main()

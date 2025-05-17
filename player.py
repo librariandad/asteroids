1f75a55ac26fcb3e42c33740f8c5eb12b6290b47
librariandad <kengwall@gmail.com>
2025-05-17 15:15:06 -0500

    delay shot

--- player.py
+++ player.py
@@ -6,9 +6,12 @@ from shot import Shot
 
 class Player(CircleShape):
     containers = None
+
+
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 180
+        self.shot_timer = 0
 
     # in the player class
     def rotate(self, frame_rate):
@@ -20,9 +23,11 @@ class Player(CircleShape):
 
     def shoot(self):
         # create a new shot
-        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
-        shot.rotation = self.rotation
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+        if self.shot_timer <= 0:
+            shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+            shot.rotation = self.rotation
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+            self.shot_timer = PLAYER_SHOT_COOLDOWN
 
 
     def triangle(self):
@@ -49,3 +54,5 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+
+        self.shot_timer -= dt

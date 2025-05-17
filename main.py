9362265d014f0abb48a6fdd5a86a988a73a6b850
librariandad <kengwall@gmail.com>
2025-05-17 09:21:31 -0500

    added player object to main loop

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pygame
 from constants import *
+from circleshape import CircleShape
+from player import Player
+
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -16,10 +17,10 @@ def main():
                 pygame.quit()
                 return
 
-        screen.fill((0, 0, 0))
-        pygame.Surface.fill(screen,(0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000.0
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
